ed3edf4f05d12bd3329d02491f716b11e0a52370
OmarAouini <aouini.omar93@gmail.com>
2021-06-20 00:17:18 +0200

    fmt and removed unwrap for ?

--- core/src/clients/rest_client.rs
+++ core/src/clients/rest_client.rs
@@ -1,19 +1,15 @@
-use ureq::{Agent, AgentBuilder, Error};
 use std::time::Duration;
+use ureq::{Agent, AgentBuilder, Error};
 
-
-pub fn rest_call(url :&str) ->  Result<String, Error> {
-
+pub fn rest_call(url: &str) -> Result<String, Error> {
     println!("starting the REST call...\n");
 
-  let agent: Agent = AgentBuilder::new()
-      .timeout_read(Duration::from_secs(5))
-      .timeout_write(Duration::from_secs(5))
-      .build();
+    let agent: Agent = AgentBuilder::new()
+        .timeout_read(Duration::from_secs(5))
+        .timeout_write(Duration::from_secs(5))
+        .build();
 
-  let body: String = agent.get(url)
-      .call()?
-      .into_string()?;
-    
+    let body: String = agent.get(url).call()?.into_string()?;
+
     Ok(body)
 }
--- core/src/main.rs
+++ core/src/main.rs
@@ -1,11 +1,10 @@
-use std::{env, process::exit};
 use clients::rest_client;
 use json_color::{Color, Colorizer};
+use std::{env, process::exit};
 
 pub mod clients;
 
 fn main() {
-
     let args: Vec<String> = env::args().collect();
 
     if args.get(1).is_none() {
@@ -13,12 +12,11 @@ fn main() {
         exit(-1)
     }
 
-    let command = args.get(1).unwrap().as_str();
+    let command = args.get(1)?.as_str();
 
     if command.eq("-r") || command.eq("-R") {
-
-    //json color config
-    let colorizer = Colorizer::new()
+        //json color config
+        let colorizer = Colorizer::new()
             .null(Color::Purple)
             .boolean(Color::Magenta)
             .number(Color::Red)
@@ -26,25 +24,24 @@ fn main() {
             .key(Color::Blue)
             .build();
 
-    let second_arg = args.get(2);
-    if second_arg.is_some() {
-
-        //REST call
-        match rest_client::rest_call(second_arg.unwrap()) {
-            Ok(value) => {
-                let colorized_value = colorizer.colorize_json_str(&value);
-                println!("{}", format!("Values: {}", colorized_value.unwrap()))
-            },
-            Err(e) => {println!("{}", format!("Error: {}", e))},
+        let second_arg = args.get(2);
+        if second_arg.is_some() {
+            //REST call
+            match rest_client::rest_call(second_arg?) {
+                Ok(value) => {
+                    let colorized_value = colorizer.colorize_json_str(&value);
+                    println!("{}", format!("Values: {}", colorized_value?))
+                }
+                Err(e) => {
+                    println!("{}", format!("Error: {}", e))
+                }
+            }
+        } else {
+            println!("the url arg is missing!\n use -h for help")
         }
+    } else if command.eq("-h") || command.eq("-H") {
+        println!("the following commands are avaliable:\n -r --> REST api call\n -h --> show help\nexample for -r: ./ccurl -r https://jsonplaceholder.typicode.com/todos/1")
     } else {
-        println!("the url arg is missing!\n use -h for help")
+        println!("error, unknown parameters!\nuse -h for help")
     }
-    
-} else if command.eq("-h") || command.eq("-H") {
-    println!("the following commands are avaliable:\n -r --> REST api call\n -h --> show help\nexample for -r: ./ccurl -r https://jsonplaceholder.typicode.com/todos/1")
-} else {
-    println!("error, unknown parameters!\nuse -h for help")
-}
-    
 }
